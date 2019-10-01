1ce7f57cb333fcf598b39c33c8113461b795d90a
Yifan Yue <yue5@cs.ualberta.ca>
2019-09-30 19:14:35 -0600

    new file

--- star.java
+++ star.java
@@ -1,2 +1,14 @@
-public class star extends Shape{}
+public class star extends Shape{
+	private String name;
+	public star(String name){
+		this.name = name;
+	}
+	public void getName(){
+		return this.name;
+	}
+	public void setNmae(String name){
+		this.name = name;
+	}
+
+}
 
