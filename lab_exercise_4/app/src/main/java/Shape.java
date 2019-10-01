9e94d87f4f7a5ab657cf240d861f97b4ff41e20c
Qian Yu <529823104@qq.com>
2019-09-30 22:28:30 -0600

    Update Shape.java

--- lab_exercise_4/app/src/main/java/Shape.java
+++ lab_exercise_4/app/src/main/java/Shape.java
@@ -19,24 +19,4 @@ public class Circle extends Shape{
 	public Circle(){;
 	}
 }
-=======
-public abstract class Shape{
-	private String color = "red";
-	private int x,y;
-	public Shape(String color){
-		this.color = color;
-	}
-	public String getColor(){
-		return this.color;
-	}
-	public void setColor(String color){
-		this.color = color;
-	}
-}
 
-public class Circle extends Shape{
-	private int x,y;
-	public Circle(){;
-	}
-}
->>>>>>> d5ff860d4aa0c1736c6ab0741ac9ae2b73d404b1
