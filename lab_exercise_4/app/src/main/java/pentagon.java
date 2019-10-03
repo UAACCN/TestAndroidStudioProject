56f282839193f7d7165ef96051032de0d8e243c5
ggboy911 <55257478+ggboy911@users.noreply.github.com>
2019-10-02 19:06:56 -0600

    Add files via upload

---
+++ lab_exercise_4/app/src/main/java/pentagon.java
@@ -0,0 +1,13 @@
+public class pentagon extends Shape{
+	private String name = "";
+	pentagon(String name){
+		this.name = name;
+	}
+	public String getName(){
+		return this.name;
+	}
+	public void setName(String name){
+		this.name = name;
+	}
+
+}
